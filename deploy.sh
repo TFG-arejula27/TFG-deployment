6ff82597b427b6e723c4bc1ced596e98a4b00895
Íñigo Aréjula Aísa <arejula27@gmail.com>
2022-02-17 11:34:13 +0100

    works

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,20 @@ kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/na
 
 helm repo add openfaas https://openfaas.github.io/faas-netes/
 
+export TIMEOUT=2m
+
 helm repo update \
  && helm upgrade openfaas --install openfaas/openfaas \
     --namespace openfaas  \
     --set functionNamespace=openfaas-fn \
     --set generateBasicAuth=true \
-    --set ingress.enabled=true
+    --set ingress.enabled=true \
+    --set gateway.upstreamTimeout=$TIMEOUT \
+  	--set gateway.writeTimeout=$TIMEOUT \
+  	--set gateway.readTimeout=$TIMEOUT \
+  	--set faasnetes.writeTimeout=$TIMEOUT \
+  	--set faasnetes.readTimeout=$TIMEOUT \
+  	--set queueWorker.ackWait=$TIMEOUT
 
 
 PASSWORD=$(kubectl -n openfaas get secret basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode) && \
@@ -33,10 +41,14 @@ sleep 40 #wait until service is up
 
 kubectl port-forward -n openfaas svc/gateway 8080:8080 &
 
+sleep 5
 OPENFAAS_URL="127.0.0.1:8080"
 
-echo -n $PASSWORD | faas-cli login -g $OPENFAAS_URL -u admin --password-stdin
+faas-cli login -g ${OPENFAAS_URL} -u admin --password ${PASSWORD}
 faas-cli version
 
 #grafana
 kubectl apply -f ./grafana.yaml
+
+sleep 10
+k port-forward svc/grafana 3000:3000 &
